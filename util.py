76ff2955f8168d1e2d7a8d38a75792ce5e903954
Shane Claussen <sclaussen@yahoo.com>
2021-01-16 11:41:52 -0800

    Updated util

--- util.py
+++ util.py
@@ -1,9 +1,22 @@
+# prompt: string to use as the prompt
+# options: array of valid responses to the prompt
 def getIntInput(prompt, options):
-    print()
-    while True:
-        response = input(prompt + " (Options: " + ", ".join(options) + "): ")
-        if response not in options:
-            print("Error: Invalid option: " + response)
-            continue
-        break
-    return int(response)
+	print()
+	while True:
+		response = input(prompt + " (Options: " + ", ".join(options) + "): ")
+		if response in options:
+			break
+			print("Error: Invalid option: " + response)
+	return int(response)
+
+
+# prompt: string to use as the prompt
+# options: array of valid responses to the prompt
+def getStringInput(prompt, options):
+	print()
+	while True:
+		response = input(prompt + " (Options: " + ", ".join(options) + "): ")
+		if response in options:
+			break
+			print("Error: Invalid option: " + str(string))
+	return response
